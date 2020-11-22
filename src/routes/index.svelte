81de28c9ef12e3b49cd553ceb91765da3a4b2172
Lawrence N. Masullo <lmasullo@gmail.com>
2020-11-21 22:15:24 -0600

    Split

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -51,7 +51,6 @@
     <a href="##">About</a>
     <a href="##">Contact</a>
   </nav>
-
 </header>
 
 <main class="layout">
@@ -64,9 +63,33 @@
       tempor
     </p>
     <button>Accept</button>
-
   </div>
   <hr />
+
+  <div class="split">
+    <aside>
+      <p>
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum.
+      </p>
+    </aside>
+
+    <p>
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+      qui officia deserunt mollit anim id est laborum.
+    </p>
+  </div>
+
+  <hr />
+
   <p>
     Lorem ipsum dolor sit amet,
     <a href="##">consectetur</a>
